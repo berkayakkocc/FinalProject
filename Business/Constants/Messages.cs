6221fd1da01bf8bb46a6a29390a2d2dbc44cdd28
berkayakkocc <77438994+berkayakkocc@users.noreply.github.com>
2022-08-11 10:58:44 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductDeleted = "Product Deleted";
+        public static string ProductUpdated= "Product Updated";
+        public static string ProductNameInvalid= "Product Name Invalid";
+
+        internal static string MaintenanceTime = "System Maintenance Time";
+
+        internal static string ProductListed = "Products listed";
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,66 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+            //GetProductDetails();
+        }
+
+        private static void GetProductDetails()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            var result = productManager.GetProductDetails();
+            if (result.Success)
+            {
+                foreach (var item in result.Data)
+                {
+                    Console.WriteLine(item.ProductName + " / " + item.CategoryName);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetAll();
+            Console.WriteLine(result.Data);
+            //if (result.Success)
+            //{
+            //    foreach (var item in result.Data)
+            //    {
+            //        Console.WriteLine(item.ProductName);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
+
+
+        }
+    }
+}
