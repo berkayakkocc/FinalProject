5be0b0f27ec7271bf1340c94fce0fd85bebf2cf7
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2022-09-04 11:46:25 +0300

    Token Helper
    Access Token ekleme yapısı

--- Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace Core.Utilities.Security.Encryption
 {
    public class SigningCredentialsHelper
     {
-        public static SigningCredentials CreateSigningCredentials(s)
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        }
     }
 }
